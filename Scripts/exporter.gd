a04ce92cf9a8fb2ab8213fa5995dbd31d9c34ba2
VPJP <vpjpeltonen@gmail.com>
2020-02-21 17:36:13 +0200

    work on saving maps

---
+++ Scripts/exporter.gd
@@ -0,0 +1,33 @@
+extends Node
+
+func screenshot():
+	var dir = Directory.new()
+	dir.open("user://")
+	dir.make_dir("sad")
+	var image = get_viewport().get_texture().get_data()
+	image.flip_y()
+	image.save_png("user://sad/screenshot.png")
+
+func make_map_folder():
+	var dir = Directory.new()
+	dir.open("user://")
+	dir.make_dir($save_box/map_name_input.text)
+	
+func save_maps():
+	var image = get_viewport().get_texture().get_data()
+	image.flip_y()
+	image.save_png("user://"+$save_box/map_name_input.text+"/screenshot.png")
+
+func _on_screenshot_button_pressed():
+	screenshot()
+
+func _on_save_button_pressed():
+	if $save_box/map_name_input.text == "":
+		$warning.dialog_text = "You need to give the map a name"
+		$warning.show()
+	else:
+		make_map_folder()
+		save_maps()
+		$warning.dialog_text = "Map saved succefully"
+		$warning.show()
+	#screenshot()
