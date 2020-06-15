7a193a3cfcca2085e3eec8e0563422ad2db6702e
VPJP <vpjpeltonen@gmail.com>
2020-06-15 17:22:08 +0300

    back button to generation menu

--- Scripts/UI Scripts/UI_controller.gd
+++ Scripts/UI Scripts/UI_controller.gd
@@ -10,3 +10,16 @@ func _on_FlagGenerator_back():
 	get_node("OpeningMenu").show()
 	get_node("info_label").show()
 	get_node("FlagGenerator").hide()
+
+
+func _on_makemap_button_pressed():
+	get_node("OpeningMenu").hide()
+	get_node("generation_UI").show()
+	get_node("generate_button").show()
+	get_node("generation_back_button").show()
+
+func _on_generation_back_button_pressed():
+	get_node("OpeningMenu").show()
+	get_node("generation_UI").hide()
+	get_node("generate_button").hide()
+	get_node("generation_back_button").hide()
